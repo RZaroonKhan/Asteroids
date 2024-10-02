ee1c052398edbe82982fa9aa0f5a830b51d0ba90
Raja Khan <rzaroonkhan@gmail.com>
2024-10-02 14:11:25 +0100

    add shot and asteroids

--- main.py
+++ main.py
@@ -1,44 +1,55 @@
-import pygame # type: ignore
-from constants import * 
-from player import *
+import sys
+import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init()
-    print('Starting asteroids!')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
-
-    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        for sprite in updatable:
-            sprite.update(dt)
-        
 
-        screen.fill('black')
-        for sprite in drawable:
-            sprite.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
-        clock.tick(60)
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-        
-        
-
-
 
 
 if __name__ == "__main__":
